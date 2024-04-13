dc827f3750c61e2b07e2e73b482f3211f1b1a7b0
casulit <chrisgelosulit@gmail.com>
2024-04-13 15:43:21 +0800

    feat(modals): Add property listing UI components

--- app/(modals)/property-listings.tsx
+++ app/(modals)/property-listings.tsx
@@ -1,10 +1,25 @@
-import { Ionicons, SafeAreaView, Text, View } from "@/components/Themed";
+import {
+  Ionicons,
+  MaterialCommunityIcons,
+  SafeAreaView,
+  Text,
+  View,
+} from "@/components/Themed";
 import Colors from "@/constants/Colors";
 import { defaultStyles } from "@/constants/Styles";
 import { useRouter } from "expo-router";
 import React from "react";
-import { Pressable, TouchableOpacity, useColorScheme } from "react-native";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  TouchableOpacity,
+  useColorScheme,
+} from "react-native";
+import PagerView from "react-native-pager-view";
 import Animated from "react-native-reanimated";
+
+const IMAGE_HEIGHT = 300;
 
 const PropertyListingsPage = () => {
   const router = useRouter();
@@ -99,7 +114,15 @@ const PropertyListingsPage = () => {
         ]}
       >
         <Pressable onPress={() => console.log("Filter press")}>
-          <Text>Filter</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 4,
+            }}
+          >
+            <Ionicons name="filter-outline" size={16} />
+            <Text>Filter</Text>
+          </View>
         </Pressable>
         <View
           style={{
@@ -135,10 +158,204 @@ const PropertyListingsPage = () => {
         showsVerticalScrollIndicator={false}
         scrollEventThrottle={16}
       >
-        <Text>a</Text>
+        <View
+          style={[
+            {
+              borderWidth: StyleSheet.hairlineWidth,
+              borderRadius: 8,
+              borderColor:
+                colorScheme === "light"
+                  ? Colors.common.gray["300"]
+                  : Colors.common.gray["600"],
+            },
+            styles.propertyListingContainerShadow,
+          ]}
+        >
+          <PagerView
+            style={[defaultStyles.container, { height: IMAGE_HEIGHT }]}
+            initialPage={0}
+          >
+            <Image
+              style={{
+                height: IMAGE_HEIGHT,
+                borderTopLeftRadius: 8,
+                borderTopRightRadius: 8,
+              }}
+              defaultSource={require("@/assets/images/real-state/douglas-sheppard-9rYfG8sWRVo-unsplash.jpg")}
+            />
+            <Image
+              style={{
+                height: IMAGE_HEIGHT,
+                borderTopLeftRadius: 8,
+                borderTopRightRadius: 8,
+              }}
+              defaultSource={require("@/assets/images/real-state/avi-werde-8N46xC5YmKM-unsplash.jpg")}
+            />
+            <Image
+              style={{
+                height: IMAGE_HEIGHT,
+                borderTopLeftRadius: 8,
+                borderTopRightRadius: 8,
+              }}
+              defaultSource={require("@/assets/images/real-state/bailey-anselme-Bkp3gLygyeA-unsplash.jpg")}
+            />
+          </PagerView>
+          <View style={{ padding: 16, gap: 8 }}>
+            <View
+              style={{ flexDirection: "row", gap: 8, alignItems: "center" }}
+            >
+              <View
+                style={{
+                  padding: 6,
+                  borderRadius: 6,
+                  borderWidth: StyleSheet.hairlineWidth,
+                  borderColor:
+                    colorScheme === "light"
+                      ? Colors.common.gray["400"]
+                      : Colors.common.gray["600"],
+                }}
+              >
+                <Ionicons name="image-outline" size={20} />
+              </View>
+              <View
+                style={{
+                  padding: 6,
+                  borderRadius: 6,
+                  borderWidth: StyleSheet.hairlineWidth,
+                  borderColor:
+                    colorScheme === "light"
+                      ? Colors.common.gray["400"]
+                      : Colors.common.gray["600"],
+                }}
+              >
+                <MaterialCommunityIcons name="floor-plan" size={20} />
+              </View>
+              <View
+                style={{
+                  padding: 6,
+                  borderRadius: 6,
+                  borderWidth: StyleSheet.hairlineWidth,
+                  borderColor:
+                    colorScheme === "light"
+                      ? Colors.common.gray["400"]
+                      : Colors.common.gray["600"],
+                }}
+              >
+                <Ionicons name="videocam-outline" size={20} />
+              </View>
+            </View>
+            <View style={[defaultStyles.removedBackground, { gap: 4 }]}>
+              <Text numberOfLines={1} fontWeight="semibold">
+                House for sale in quezon city business district
+              </Text>
+              <Text>Quezon City, NCR</Text>
+            </View>
+            <View style={[defaultStyles.removedBackground, { gap: 4 }]}>
+              <Text numberOfLines={1} fontWeight="semibold" fontSize={18}>
+                Php 2,500,000.00
+              </Text>
+              <View
+                style={{ flexDirection: "row", gap: 8, alignItems: "center" }}
+              >
+                <Text>135 sqm</Text>
+                <Text>1,135 price/sqm</Text>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              borderBottomWidth: StyleSheet.hairlineWidth,
+              ...borderColor,
+            }}
+          />
+          <View
+            style={[
+              defaultStyles.removedBackground,
+              {
+                padding: 16,
+                alignItems: "center",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              },
+            ]}
+          >
+            <View
+              style={{
+                gap: 12,
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                style={{ flexDirection: "row", gap: 4, alignItems: "center" }}
+                activeOpacity={0.75}
+              >
+                <Ionicons
+                  name="call"
+                  size={20}
+                  lightColor={Colors.light.primary}
+                  darkColor={Colors.dark.primary}
+                />
+                <Text
+                  fontWeight="semibold"
+                  lightColor={Colors.light.primary}
+                  darkColor={Colors.dark.primary}
+                >
+                  Call
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{ flexDirection: "row", gap: 4, alignItems: "center" }}
+                activeOpacity={0.75}
+              >
+                <Ionicons
+                  name="chatbox-ellipses"
+                  size={20}
+                  lightColor={Colors.light.primary}
+                  darkColor={Colors.dark.primary}
+                />
+                <Text
+                  fontWeight="semibold"
+                  lightColor={Colors.light.primary}
+                  darkColor={Colors.dark.primary}
+                >
+                  Chat
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{ flexDirection: "row", gap: 12, alignItems: "center" }}
+            >
+              <Ionicons
+                name="trash-outline"
+                size={20}
+                lightColor={Colors.light.primary}
+                darkColor={Colors.dark.primary}
+              />
+              <Ionicons
+                name="heart-outline"
+                size={20}
+                lightColor={Colors.light.primary}
+                darkColor={Colors.dark.primary}
+              />
+            </View>
+          </View>
+        </View>
       </Animated.ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  propertyListingContainerShadow: {
+    elevation: 1,
+    shadowOpacity: 0.04,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+});
 
 export default PropertyListingsPage;
